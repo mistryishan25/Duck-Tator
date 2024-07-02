5105668d01a2e2d4810f3ff44ea3bc9149c2f405
ihmistry <ihmistry@ncsu.edu>
2024-07-02 17:14:32 -0600

    imgs and txt altered succesfully

--- content.js
+++ content.js
@@ -9,24 +9,79 @@ let gudetama_images = [
     // Add more direct image URLs as needed
 ];
 
+let phrases = [ 
+    "I think you're drowning in a sea of information."
+    // , "I guess I'm destined to be eaten."
+    // , "If the most important person in the country is like this, I fear for the future."
+    // , "When people say 'complicated,' there's usually a childish reason behind it."
+    // , "The economic outlook is eggcellent."
+    // , "This is better than watching grass grow"
+    // , "We've had our ups and downs but today's a perfect day for a nap"
+    // , "When I knock back some soy sauce, all my misery just melts away"
+    // , "It ain't like I wanna be rotten."
+    // , "The same way you win someone's heart. If you want it… you steal it."
+    // , "In the end, you can only live in the world you see."
+    // , "I was able to come this far because you were with me. We're seeing this through to the end together!"
+    // , "I'm still raw. I haven't even had salt sprinkled on me."
+    // "what a draggggg...",
+    // "Pah!”,“Ugh",
+    // "Never mind",
+    // "Leave me alone",
+    // "I cant",
+    // "Seriously, I cant." 
 
-function updateImages() {
-    const imgs = document.getElementsByTagName("img");
-    for (let image of imgs) {
-        const random_img = Math.floor(Math.random() * gudetama_images.length);
-        image.src = gudetama_images[random_img];
-    }
+]
+
+
+
+function debounce(func, delay) {
+    let debounceTimer;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
 }
 
-// Initial image update
-updateImages();
+function updateContent() {
+    function changeImages() {
+        const imgs = document.getElementsByTagName("img");
+        for (let image of imgs) {
+            const random_img = Math.floor(Math.random() * gudetama_images.length);
+            image.src = gudetama_images[random_img];
+        }
+    }
 
-// Interval update to catch any images loaded after initial page load (every 10 seconds)
-setInterval(updateImages, 1000);
+    function changeText() {
+        const textTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li'];
+        for (let tag of textTags) {
+            const elements = document.getElementsByTagName(tag);
+            for (let element of elements) {
+                const random_phrase = Math.floor(Math.random() * phrases.length);
+                element.textContent = phrases[random_phrase];
+            }
+        }
+    }
 
-// MutationObserver to watch for new images added to the DOM
-const observer = new MutationObserver(updateImages);
-observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-});
+    // Initial content update
+    changeImages();
+    changeText();
+
+    // Debounced function for updating images and text
+    const debouncedUpdate = debounce(() => {
+        changeImages();
+        changeText();
+    }, 500);  // Adjust the delay as needed
+
+    // MutationObserver to watch for new content added to the DOM
+    const observer = new MutationObserver(() => {
+        debouncedUpdate();
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
+
+updateContent();
